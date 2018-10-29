3f8af4e441995c4e88a0331d2e915e92a50ea670
agolbert <agolbert@magicleap.com>
2018-10-29 07:55:51 +0200

    working with rank loss

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+import PIL
 import shutil
 import itertools
 import numpy as np
@@ -10,58 +11,128 @@ import pandas as pd
 import yaml
 from torch.autograd import Variable
 import torchvision.transforms as transforms
-#import torchvision.datasets as datasets
 from torch.utils.data import Dataset
+import ipdb
+from sklearn.metrics import average_precision_score as MAP
 
 config_file = 'data/config.yaml'
+
+def create_config_file(csv_file_name):
+    dat = pd.read_csv(csv_file_name)
+    #dat = dat.rename(str.strip, axis='columns')
+    #dat = dat.rename(lambda x : x.replace(' ',''), axis='columns')
+
+    remap_keys = ['general_class', 'sub_class','color']
+    remap={}
+    for k in dat.keys()[-15:]:
+        vals=np.unique(dat[k])
+        d = {}
+        for i,v in enumerate(vals):
+            #dat[dat==v]=i
+            if type(v) is np.int64:
+                d[str(v)]=int(max(0,v))
+            else:
+                assert type(v) is str, (k,v,type(v))
+                d[v]=i
+        remap[k]=d
+    yaml.dump(remap, open('data/config.yaml','w' ))
+
+class PredictionCollector(object):
+    def __init__(self, keys, criterion=None):
+        self.output = pd.DataFrame(columns=keys)
+        self.labels = pd.DataFrame(columns=keys)
+        if criterion is not None:
+            self.criterion = criterion
+        else:
+            self.criterion = nn.BCEWithLogitsLoss()):
+
+    def add(self, tagids, predictions,labels=None):
+        tmp = pd.DataFrame(index=tagids,columns=self.output.keys())
+        tmp.loc[tagids]=predictions
+        self.output = self.output.append(tmp)
+        print 'current predcition size = ', self.output.shape
+        if labels is not None:
+            tmp = pd.DataFrame(index=tagids,columns=self.output.keys())
+            tmp.loc[tagids]=labels
+            self.labels = self.output.append(tmp)
+
+    def calc_map(self):
+        y_true = self.lables.values
+        y_score = self.output.values
+        mask = np.count_nonzero(y_true,axis=0)>0
+        return MAP(y_true[:,mask],y_score[:,mask],average=None)
+
+    def calc_loss(self, criterion):
+        return self.criterion(nutputs, Y)
+
+    def save(self, csv_filename):
+        by_prob = pd.DataFrame(index=self.output.index,columns=self.output.keys())
+        for key in by_prob.keys():
+            inds = np.argsort(self.output[key])[::-1]
+            assert np.all(inds>=0)
+            by_prob[key] = self.output.index[inds].copy()
+            #if 'small' in key or 'large' in key:
+            #    print self.output[key]
+            #    print inds
+            #    print self.output.index[inds]
+            #    import ipdb; ipdb.set_trace()
+        by_prob.to_csv(csv_filename)
+        return by_prob
+
 class MafatDataset(Dataset):
-    def __init__(self, csv_file_name, preload=False):
+    def __init__(self, csv_file_name, answer_csv, imfolder, preload=False,
+            resize=True, patch_size=128, full_size=224, augment=True, start=0, end=1):
+        """
+        croping scheme:
+        patch_size is the size of pixels containing data.
+        full size is the patch size that will be given to the next.
+        The diff between patch_size and full size will be black pixels
+        resize False will simply crop a pathc of patch size oround the center of the detections
+        resize False will take the detection (+5 pixels) and resize to patch_size
+        """
         super(MafatDataset, self).__init__()
-        self.transforms = transforms.Compose([
+        self.patch_size=224
+        self.resize=resize
+        self.patch_size=patch_size
+        self.full_size=full_size
+        if augment:
+            self.transforms = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.RandomHorizontalFlip(),
+                #transforms.RandomVerticalFlip(),
+                #transforms.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                transforms.RandomRotation(25),
+                transforms.ToTensor()
+                ])
+        else:
+            self.transforms = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.ToTensor() ])
+        self.trans_final = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.ToTensor()
-            ])
+            transforms.Resize(self.patch_size, PIL.Image.BICUBIC),
+            transforms.Pad((self.full_size-self.patch_size)/2),
+            transforms.ToTensor()])
+        self.top_class = ['general_class', 'sub_class', 'color']
         self.dat = pd.read_csv(csv_file_name)
-        self.dat = self.dat.rename(str.strip, axis='columns')
-        self.dat = self.dat.rename(lambda x : x.replace('_',''), axis='columns')
-        self.dat = self.dat.rename(lambda x : x.replace(' ',''), axis='columns')
+        self.answer = pd.read_csv(answer_csv)
 
-        self.imfolder = os.path.join(os.path.dirname(csv_file_name), 'imagery/%d.*')
-
-        remap_keys = ['generalclass', 'subclass','color']
-        self.remap={}
-        for k in self.dat.keys()[-15:]:
-            vals=np.unique(self.dat[k])
-            d = {}
-            for i,v in enumerate(vals):
-                #self.dat[self.dat==v]=i
-                if type(v) is np.int64:
-                    d[str(v)]=int(max(0,v))
-                else:
-                    assert type(v) is str
-                    d[v]=i
-            self.remap[k]=d
-        yaml.dump(self.remap, open('data/config.yaml','w' ))
+        self.imfolder = os.path.join(imfolder, '%d.*')
+        assert os.path.exists('data/config.yaml')
         self.remap = yaml.load(open('data/config.yaml','r' ))
-        #print self.remap
-        #print self.dat
-        for k,sub_dict in self.remap.items():
-            for val, sub_class in sub_dict.items():
-                col = self.dat[k]
-                col[col==sub_class]=val
-        self.dat.to_csv('data/train_clean.csv')
+
         self.ims={}
 
         if preload:
-            imageids = np.unique(self.dat['imageid'])
+            imageids = np.unique(self.dat['image_id'])
+            if start is not None and end is not None:
+                imageids = imageids[int(len(imageids)*start):int(len(imageids)*end)]
             for id in imageids:
                 imfile = self.imfolder%id
                 imfile = glob.glob(imfile)[0]
                 assert id not in self.ims
-                self.ims[id]=cv2.imread(imfile)
-                self.ims[id] = cv2.cvtColor(cv2.imread(imfile), cv2.COLOR_BGR2RGB)
+                #self.ims[id] = cv2.cvtColor(cv2.imread(imfile), cv2.COLOR_BGR2RGB)
+            self.dat = self.dat.loc[np.isin(self.dat['image_id'], imageids)]
 
     def __len__(self):
         return len(self.dat)
@@ -72,50 +143,24 @@ class MafatDataset(Dataset):
         return vec
 
     def row_to_label(self, row):
-        top_class = '''generalclass subclass color'''.split()
-        '''sunroof luggage_carrier open_cargo_area
-        enclosed_cab    spare_wheel wrecked flatbed ladder  enclosed_box
-        soft_shell_box  harnessed_to_a_cart ac_vents    color'''
-        #for  row.values.ravel()[-15:]
-        label = []
-        for k,d in self.remap.items():
-            value = row[k].values[0]
-            if k in top_class:
-                label.append(self.encode(len(np.unique(d.values())), d[value]))
-            else:
-                label.append([int(int(value)>0)])
+        labels = pd.DataFrame(columns=self.answer.keys())
+        labels.loc[0] = np.zeros(len(labels.keys()))
 
-        labels = np.hstack(label).astype(np.float32)
-        return labels
+        for k,d in self.remap.items():
+            if k in row:
+                value = row[k].values[0]
+                if k in self.top_class:
+                    labels[value].loc[0]=1
+                else:
+                    labels[k].iloc[0]=int(value>0)
+
+        return labels.values.ravel().astype(np.float32)
 
     def get_class_names(self):
-        top_class = '''generalclass subclass color'''.split()
-        text = []
-        for k,d in self.remap.items():
-            if k in top_class:
-                text.extend(d.keys())
-            else:
-                text.append(k)
-        return text
+        return self.answer.keys()
 
     def labels_to_text(self, labels):
-        top_class = '''generalclass subclass color'''.split()
-        counter = 0
-        text = []
-        for k,d in self.remap.items():
-            if k in top_class:
-                n = len(np.unique(d.values()))
-                value = np.argmax(labels[counter:counter+n])
-                counter+=n
-                name = [name for name,v in d.items() if v == value]
-                text.extend(name)
-            else:
-                value = labels[counter]
-                counter+=1
-                if value > 0.5:
-                    text.append(k)
-        assert counter==len(labels)
-        return ', '.join(text)
+        return ', '.join([self.answer.keys()[i] for i,v in enumerate(labels) if v>0.5])
 
     def crop(self, im, x, y, size=112):
         if size<x<im.shape[1]-size and size<y<im.shape[0]-size:
@@ -131,37 +176,41 @@ class MafatDataset(Dataset):
 
     def __getitem__(self, index):
         row = self.dat.iloc[[index]]
-        print row
-        imageid = row['imageid'].values[0]
+        imageid = row['image_id'].values[0]
         if imageid in self.ims:
             im = self.ims[imageid]
         else:
             imfile = self.imfolder%imageid
             imfile = glob.glob(imfile)[0]
             im = cv2.cvtColor(cv2.imread(imfile), cv2.COLOR_BGR2RGB)
-            
-        xs = ['p%dx'%i for i in range(1,5)]
-        ys = ['p%dy'%i for i in range(1,5)]
+        xs = ['p1_x', ' p2_x', ' p3_x', ' p4_x']
+        ys = ['p_1y', ' p2_y', ' p3_y', ' p4_y']
 
-        x = np.mean(row[xs].values)
-        y = np.mean(row[ys].values)
-        I = self.crop(im,int(x),int(y))
+        dx = np.max(row[xs].values)-np.min(row[xs].values) + 10
+        dy = np.max(row[ys].values)-np.min(row[ys].values) + 10
+        patch_size = np.sqrt(dx**2+dy**2)+10
+        if not self.resize:
+            patch_size = np.maximum(self.patch_size, patch_size)
+
+        tmp_half_size = int(1.5*patch_size/2)
+        center_x = np.mean(row[xs].values)
+        center_y = np.mean(row[ys].values)
+        I = self.crop(im,int(center_x),int(center_y),tmp_half_size)
         I = self.transforms(I)
 
+        y = int(tmp_half_size+np.random.rand()*5)
+        x = int(tmp_half_size+np.random.rand()*5)
+        half_size = int(patch_size/2)
+        I = I[:, y-half_size:y+half_size,x-half_size:x+half_size]
+        I = self.trans_final(I)
+
         labels = self.row_to_label(row)
+        assert I.shape[-1]==224, (I.shape)
+        assert I.shape[-2]==224, (I.shape, index)
 
-        return (I, labels, '%d,%d,%s'%(index,imageid,self.labels_to_text(labels)))
+        return (I, labels, '%d,%d,%s'%(row['tag_id'],imageid,self.labels_to_text(labels)))
 
-
-if __name__=='__main__':
-    data = MafatDataset('data/train/train.csv')
-    print len(data)
-    for i in [49]:
-        im, l, text = data.__getitem__(i)
-        print 'label', l
-        print text
-        assert len(l)==37
-        import ipdb; ipdb.set_trace()
-        import matplotlib.pyplot as plt
-        plt.imshow(im[0])
-        plt.show()
+def create_train_val_dataset(csv_file_name, answer_csv, imfolder, split=0.8, preload=True):
+    train = MafatDataset('data/train.csv', 'data/answer.csv', 'data/training imagery', preload, start=0, end=0.8)
+    val = MafatDataset('data/train.csv', 'data/answer.csv', 'data/training imagery', preload, start=0.8, end=1, augment=False)
+    return train, val
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import numpy as np
+import os
 import cv2
 import argparse
 import torchvision.models as models
@@ -7,10 +9,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-from data_loader import MafatDataset
+from data_loader import MafatDataset, PredictionCollector, create_train_val_dataset
 from tensorboardX import SummaryWriter
 from PIL import Image
 from sklearn.metrics import average_precision_score as MAP
+from loss import RankLoss
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 #parser.add_argument('data', metavar='DIR', help='path to dataset')
@@ -22,13 +25,14 @@ parser.add_argument('--lr', '--learning-rate', default=0.01, type=float, metavar
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=10, type=int, metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--resume', action='store_false')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_true', help='use pre-trained model')
 parser.add_argument('--freeze', dest='freeze', action='store_true', help='freeze params for base model')
 parser.add_argument('--weighted-loss', dest='weighted_loss', action='store_true', help='use weights in log loss')
 parser.add_argument('--preload', dest='preload', action='store_false', help='preload all images into memory before training')
 parser.add_argument('--tag', type=str, default='baseline', help='tag - name of this experiment')
+parser.add_argument('--start-tag', type=str, default=None, help='start-tag - name of experiment use as pretrain')
 args = parser.parse_args()
 
 def display_images(X, text, pred_text, nrow=4):
@@ -64,111 +68,158 @@ def display_images(X, text, pred_text, nrow=4):
     return make_grid(images, nrow=4)
 
 
+def evaluate(model, dataset, output_file, writer, freq=10):
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+    model.cuda()
+    model.eval()
+    sigmoid = nn.Sigmoid()
+    collector = PredictionCollector(dataset.get_class_names())
+    with torch.no_grad():
+        for it, data in enumerate(train_loader):
+            images,labels, gt_text = data
+            X = Variable(images, volatile=True).cuda()
+            outputs = model(X)
+            prediction = sigmoid(outputs)
+            ids = [int(text.split(',')[0]) for text in gt_text]
+            collector.add(ids, prediction)
+
+            print 'display iter ', it
+            if it%freq==0:
+                pred_text = map(dataset.labels_to_text, prediction.detach().cpu().numpy()[:16])
+                grid=display_images(X[:16], gt_text[:16], pred_text[:16], nrow=4)
+                writer.add_image('Test/images', grid, it)
+        probs = collector.save(output_file)
+
+def write_to_board(writer, collector, it, dataset, it,  curr_batch,  num_steps, stage='train'):
+    outputs, images, Y, gt_text = curr_batch
+    #def write_to_board(writer, outputs, images, Y, gt_text, running_loss, num_steps, dataset, it, stage='train'):
+    """
+    it = current iteration to log
+    running_loss 
+    """
+    sigmoid = nn.Sigmoid()
+    prediction = sigmoid(outputs)
+    pred_text = map(dataset.labels_to_text, prediction.detach().cpu().numpy()[:16])
+    running_loss = running_loss.detach().cpu().numpy()
+    names = dataset.get_class_names()
+
+    y_true = Y.cpu().detach().numpy()
+    y_score = outputs.cpu().detach().numpy()
+    mask = np.count_nonzero(y_true,axis=0)>0
+    average_prec = MAP(y_true[:,mask],y_score[:,mask],average=None)
+    per_class =  dict(zip(np.array(names)[mask], average_prec))
+
+    writer.add_scalars('Loss', {'total_%s'%stage: running_loss.mean()/num_steps}, it)
+    writer.add_scalars('Loss',
+            {'%s_%s'%(name,stage):l/num_steps
+                for name,l in zip(names, running_loss)}, it)
+    writer.add_scalars('MAP', {'total_%s'%stage: average_prec.mean()}, it)
+    map_per_class = {'%s_%s'%(name,stage):per_class[name]
+                for name,l in zip(names, running_loss) if name in per_class}
+    writer.add_scalars('MAP', map_per_class , it)
+    grid=display_images(images[:16], gt_text[:16], pred_text[:16], nrow=4)
+    writer.add_image('%s/images'%stage, grid, it)
+
+    for key, value in sorted(map_per_class.iteritems(), key=lambda (k,v): (v,k)):
+        print "MAP %s: %s" % (key, value)
+
+
 def main():
     model = models.resnet18(pretrained=True)
-    assert not args.freeze, 'not yet supported'
+    if args.freeze:
+        for param in model.parameters():
+            param.requires_grad = False
     assert not args.weighted_loss, 'not yet supported'
-    model.fc = torch.nn.Linear(in_features=512, out_features=37, bias=True)
+    model.fc = nn.Sequential( torch.nn.Linear(in_features=512, out_features=1024),
+            torch.nn.Linear(in_features=1024, out_features=37))
     cofga_v0 = model.cuda()
-    #nn.Sequential(model, nn.Sigmoid()).cuda()
 
     criterion = nn.BCEWithLogitsLoss(reduction='none')
-    optimizer = optim.SGD(cofga_v0.parameters(), lr=args.lr, momentum=args.momentum)
+    #criterion = RankLoss()
+    #optimizer = optim.SGD(cofga_v0.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.Adam(cofga_v0.parameters(), lr=args.lr)
 
     filename = 'resnet18_BCEloss37%s.checkpoint.pth.tar'%args.tag
-    try:
-        checkpoint = torch.load(filename)
-        net.load_state_dict(checkpoint['state_dict'])
+    if args.start_tag is not None:
+        start_filename = 'resnet18_BCEloss37%s.checkpoint.pth.tar'%args.start_tag
+    else:
+        start_filename = filename
+
+    start_epoch = 0
+    if os.path.exists(start_filename) and args.resume:
+        checkpoint = torch.load(start_filename)
+        cofga_v0.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         start_epoch = checkpoint['epoch']
-    except:
-        throw
+    if args.start_tag is not None:
         start_epoch=0
-        print 'failed to load checkoutput'
-    train_dataset = MafatDataset('data/train/train.csv', preload=args.preload)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
-    #val_loader = torch.utils.data.DataLoader(MafatDataset('data/val/train.csv'), batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+
 
     writer = SummaryWriter('runs/renset18/%s'%args.tag)
-    for epoch in range(100):  # loop over the dataset multiple times
+    if args.evaluate:
+        output_file = 'answer_%s.csv'%args.tag
+        data = MafatDataset('data/test.csv', 'data/answer.csv', 'data/test imagery', preload=True,augment=False)
+        evaluate(model, data, output_file, writer)
+        exit()
+
+    train_dataset, val_dataset = create_train_val_dataset('data/test.csv',
+            'data/answer.csv', 'data/test imagery', preload=args.preload)
+    #train_dataset = val_dataset #TODO AVRAM
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+            batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    collector = PredictionCollector(train_dataset.get_class_names())
+    for epoch in range(start_epoch, args.epochs):  # loop over the dataset multiple times
         running_loss = None
         running_accuracy = 0.0
         for i, data in enumerate(train_loader):
+            print 'epoch %d, i %d'%(epoch,i)
             images,labels, gt_text = data
             X = Variable(images).cuda()
             outputs = cofga_v0(X)
             Y = Variable(labels).cuda()
+            ids = [int(text.split(',')[0]) for text in gt_text]
+            prediction = sigmoid(outputs)
+            collector.add(ids, outputs, Y)
             loss = criterion(outputs, Y)
             s=loss.mean()
             optimizer.zero_grad()
             s.backward()
             optimizer.step()
 
+            if len(loss.shape)>1:
+                loss = loss.sum(0)
             if running_loss is None:
-                running_loss = loss.mean(0)
+                running_loss = loss
             else:
-                running_loss += loss.mean(0)
-            print 'loss - ', loss.data.mean()
-            #accuracy, ac3, ac5, train_images_vis = metrics(Y, outputs, images)
-            #running_accuracy += accuracy
-            #running_ac3 += ac3
-            #running_ac5 += ac5
-            if i%10==0:
-                print 'running loss ', running_loss
-                sigmoid = nn.Sigmoid()
-                prediction = sigmoid(outputs)
-                pred_text = map(train_dataset.labels_to_text, prediction.detach().cpu().numpy()[:16])
-                it = epoch*len(train_loader)+i
-                writer.add_scalar('Train/loss', running_loss.mean().data/(i+1), it)
-                names = train_dataset.get_class_names()
+                running_loss += loss
 
-                y_true = Y.cpu().detach().numpy()
-                y_score = outputs.cpu().detach().numpy()
-                mask = np.count_nonzero(y_true,axis=0)>0
-                average_prec = MAP(y_true[:,mask],y_score[:,mask],average=None)
-                per_class =  dict(zip(np.array(names)[mask], average_prec))
-                print per_class
+        print 'epoch %d, running loss '%epoch, running_loss/len(train_dataset)
+        it = (epoch+1)*len(train_loader)
+        write_to_board(writer, outputs, X, Y, gt_text, running_loss,
+                len(train_dataset), train_dataset, it, stage='train')
 
-                for name,l in zip(names, running_loss.data):
-                    print name,l
-                    writer.add_scalar('Train/loss_%s'%name, l/(i+1), it)
-                    if name in per_class:
-                        writer.add_scalar('Train/map_%s'%name, per_class[name], it)
-
-
-                #writer.add_scalar('Train/accuracy', running_accuracy/(i+1), it)
-                #writer.add_scalar('Train/accuracy@3', running_ac3/(i+1), it)
-                #writer.add_scalar('Train/accuracy@5', running_ac5/(i+1), it)
-                print pred_text
-                grid=display_images(X[:16], gt_text[:16], pred_text[:16], nrow=4)
-                writer.add_image('Train/images', grid, it)
-                #val_loss = 0.0
-                #val_accuracy = 0.0
-                #val_ac3 = 0.0
-                #val_ac5 = 0.0
-                #cofga_v0.eval()
-                #for j, data in enumerate(val_loader):
-                #    inputs, labels, images = data
-                #    X = Variable(torch.cat([inputs[:,0,:,:], inputs[:,1,:,:]])).cuda()
-                #    outputs = cofga_v0(X)
-                #    Y = Variable(labels).cuda()
-                #    val_loss += criterion(outputs, Y).data
-                #    accuracy, ac3, ac5, val_images_vis = metrics(Y, outputs, images)
-                #    val_accuracy += accuracy
-                #    val_ac3 += ac3
-                #    val_ac5 += ac5
-                #writer.add_scalar('Val/loss', val_loss/(j+1), it)
-                #writer.add_scalar('Val/accuracy', val_accuracy/(j+1), it)
-                #writer.add_scalar('Val/accuracy@3', val_ac3/(j+1), it)
-                #writer.add_scalar('Val/accuracy@5', val_ac5/(j+1), it)
-                #ims = [im for im in itertools.chain(*val_images_vis)]
-                #writer.add_image('Val/TOP5', make_grid( ims, nrow=6), it)
-                #print('[%d, %d] train, val loss: %.3f, %.3f' % (epoch + 1, i + 1,
-                #    running_loss/(i+1), val_loss/(j+1)))
-                #print('         train accuracy : %.3f, val accuracy = %.3f' % ( running_accuracy / (i+1), val_accuracy/(j+1)))
-                #cofga_v0.train()
-            torch.save({ 'epoch': epoch + 1, 'state_dict': cofga_v0.state_dict(),
-                'optimizer' : optimizer.state_dict(), }, filename)
+        with torch.no_grad():
+            cofga_v0.eval()
+            running_loss=None
+            for _, data in enumerate(val_loader):
+                images,labels, gt_text = data
+                X = Variable(images).cuda()
+                outputs = cofga_v0(X)
+                Y = Variable(labels).cuda()
+                loss = criterion(outputs, Y)
+                if len(loss.shape)>1:
+                    loss = loss.sum(0)
+                if running_loss is None:
+                    running_loss = loss
+                else:
+                    running_loss += loss
+            write_to_board(writer, outputs, X, Y, gt_text,
+                    loss, len(val_dataset), val_dataset, it, stage='val')
+            cofga_v0.train()
+        running_loss=None
+        torch.save({ 'epoch': epoch + 1, 'state_dict': cofga_v0.state_dict(),
+            'optimizer' : optimizer.state_dict(), }, filename)
     print('Finished ')
 if __name__=='__main__':
     main()
